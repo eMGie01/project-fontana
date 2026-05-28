54ab8f0bbe7921f6830e8df97e24cebc37f2311b
eMGie01 <marek.galeczka@outlook.com>
2026-05-28 10:58:24 +0200

    changes in submodule

---
+++ firmware/lib/driver-sd/sd.h
@@ -0,0 +1,24 @@
+/**
+ * @file sd.h
+ * @author Marek Galeczka (marek.galeczka@outlook.com)
+ * @brief 
+ * @version 0.1
+ * @date 2026-05-28
+ * 
+ * @copyright Copyright (c) 2026
+ * 
+ */
+
+#ifndef SD_H
+#define SD_H
+
+/**
+ * open,
+ * close,
+ * mount
+ * unmount,
+ * is_mounted
+ *  
+ */
+
+#endif // SD_H
