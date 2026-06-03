8b38d00b8869742e3f525b4583cdbb82a9c1e5f3
eMGie01 <marek.galeczka@outlook.com>
2026-06-03 16:03:50 +0200

    sd update

--- firmware/lib/driver-sd/sd.c
+++ firmware/lib/driver-sd/sd.c
@@ -2,19 +2,94 @@
 
 #include <stdio.h>
 #include <string.h>
+#include <dirent.h>
+
 #include "esp_log.h"
 #include "esp_vfs_fat.h"
-#include "driver/sdspi_host.h"
+// #include "driver/sdspi_host.h"
 #include "driver/spi_common.h"
 #include "sdmmc_cmd.h"
-
 #include "hw_config.h"
 
 static const char* TAG = "SD";
 
+#define SLASH 47 // ascii value for slash
+
+static bool
+s_check_config(const sd_config_t* config)
+{
+    if (config->spi_host > SPI_HOST_MAX || config->spi_host == SPI1_HOST)
+    {
+        return false;
+    }
+    if (!GPIO_IS_VALID_GPIO(config->pin_cs))
+    {
+        return false;
+    }
+    if (config->mount_point == NULL || config->mount_point[0] != SLASH)
+    {
+        return false;
+    }
+    if (config->max_files > 256 || config->max_files <= 0)
+    {
+        return false;
+    }
+    if (config->freq_khz <= 0)
+    {
+        return false;
+    }
+    return true;
+}
+
+sd_err_t
+sd_init(const sd_config_t* config, sd_handle_t sd)
+{
+    ESP_LOGD(TAG, "sd card init");
+    if (sd == NULL || config == NULL)
+    {
+        ESP_LOGD(TAG, "invalid initial arguments");
+        return SD_ERR_INVAL_ARG;
+    }
+
+    if (sd->state != SD_STATE_UNINIT)
+    {
+        return SD_ERR_INVAL_STATE;
+    }
+
+    if (!s_check_config(config))
+    {
+        return SD_ERR_INVAL_ARG;
+    }
+
+    sd->host = SDSPI_DEFAULT_HOST();
+    sd->host.slot = config->spi_host;
+    sd->host.max_freq_khz = config->freq_khz;
+    sdspi_device_config_t dev_cfg = SDSPI_DEVICE_CONFIG_DEFAULT();
+    dev_cfg.host_id = config->spi_host;
+    dev_cfg.gpio_cs = config->pin_cs;
+    
+    esp_err_t res = sdspi_host_init_device(&dev_cfg, &sd->host.slot);
+    if (res != ESP_OK)
+    {
+        return SD_ERR_INIT_FAIL;
+    }
+
+    sd->card = NULL;
+    sd->mounted = false;
+    sd->mutex = xSemaphoreCreateMutex();
+    if (sd->mutex == NULL)
+    {
+        sdspi_host_remove_device(&sd->host.slot);
+        return SD_ERR_INIT_FAIL;
+    }
+
+    memcpy(&sd->config, config, sizeof(sd_config_t));
+
+}
+
 esp_err_t sd_mount(sd_handle_t sd, char* mp, spi_host_device_t host_id)
 {
-    if (sd == nullptr || mp == nullptr)
+    if (sd == NULL || mp == NULL)
     {
         return ESP_ERR_INVALID_ARG;
     }
@@ -33,22 +108,68 @@ esp_err_t sd_mount(sd_handle_t sd, char* mp, spi_host_device_t host_id)
     err = esp_vfs_fat_sdspi_mount(sd->mount_point, &host, &slot_config, &mount_config, sd->card);
     if (err != ESP_OK)
     {
-        sd->card = nullptr;
-        sd->mount_point == nullptr;
+        sd->card = NULL;
+        sd->mount_point == NULL;
         return ESP_FAIL;
     }
     return ESP_OK;
 }
 
-esp_err_t sd_file_create(sd_handle_t sd, const char* file_name)
+esp_err_t sd_file_create(sd_handle_t sd, char* file_name, char* file_path)
 {
-    if (sd == nullptr || sd->card == nullptr || sd->mount_point == nullptr)
+    if (sd == NULL || sd->card == NULL || sd->mount_point == NULL)
     {
         return ESP_ERR_INVALID_ARG;
     }
-    const char* fname = file_name;
-    if (fname == nullptr)
+    if (strlen(file_name) > 58)
     {
-        // looking for next file
+        return ESP_ERR_INVALID_ARG;
     }
+    DIR* d = opendir(sd->mount_point);
+    struct dirent* dir;
+    int max_index = -1;
+    char buff[64] = {0};
+    snprintf(buff, sizeof(buff), "%s_%%d.csv", file_name);
+    if (d)
+    {
+        while((dir = readdir(d)) != NULL)
+        {
+            int index;
+            if (sscanf(dir->d_name, buff, &index) == 1)
+            {
+                max_index = index;
+            }
+        }
+        closedir(d);
+    }
+    else
+    {
+        ESP_LOGD(TAG, "cannot open file for dir: %s", sd->mount_point);
+    }
+    int next_index = (max_index == -1) ? 1 : (max_index + 1);
+    char path[128];
+    snprintf(path, sizeof(path), "%s/%s_%02d.csv", sd->mount_point, file_name, next_index);
+    FILE* f = fopen(path, "w");
+    if (f != NULL)
+    {
+        ESP_LOGD(TAG, "new file created in dir: %s", path);
+        fclose(f);
+        file_path = path;
+        return ESP_OK;
+    }
+    ESP_LOGD(TAG, "creating file with path: %s failed", path);
+    return ESP_FAIL;
 }
+
+esp_err_t
+sd_file_write(sd_handle_t sd, char* file_path)
+{
+    FILE* f = fopen(file_path, "w");
+    if (f == NULL)
+    {
+        ESP_LOGD(TAG, "writing to file: %s failed", file_path);
+    }
+
+}
+
+
--- firmware/lib/driver-sd/sd.h
+++ firmware/lib/driver-sd/sd.h
@@ -21,28 +21,62 @@
 extern "C" {
 #endif
 
-/**
- * open,
- * close,
- * mount
- * unmount,
- * is_mounted
- */
+#include "driver/sdspi_host.h"
+#include "freertos/FreeRTOS.h"
+
+typedef enum
+{
+    SD_OK                   =  (0),
+    SD_ERR_INVAL_ARG        = (-1),
+    SD_ERR_INVAL_STATE      = (-2),
+    SD_ERR_INIT_FAIL        = (-3),
+    SD_ERR_MOUNT_FAIL       = (-4),
+    SD_ERR_NOT_FOUND        = (-5),
+    SD_ERR_ALREADY_EXISTS   = (-6),
+    SD_ERR_WRITE_FAIL       = (-7),
+    SD_ERR_NO_SPACE         = (-8),
+    SD_ERR_BUFF_TOO_SMALL   = (-9),
+    SD_ERR_NO_CARD          = (-10),
+} sd_err_t;
+
+typedef enum
+{
+    SD_STATE_INITED     = (0),
+    SD_STATE_MOUNTED    = (1),
+    SD_STATE_UNINIT     = (2),
+} sd_state_t;
 
 typedef struct
 {
-} sd_mount_config_t;
+    sd_mode_t mode;
+    spi_host_device_t spi_host;
+    gpio_num_t pin_cs;
+    const char* mount_point;
+    int max_files;
+    bool format_if_mount_failed;
+    size_t allocation_unit_size;
+    int freq_khz;
+    bool use_pullups;
+} sd_config_t;
 
 typedef struct
 {
     sdmmc_card_t* card;
-    char* mount_point;
-} sd_t;
-typedef sd_t* sd_handle_t;
+    sdmmc_host_t host;
+    sd_state_t state;
+    sd_config_t config;
+    bool mounted;
+    SemaphoreHandle_t mutex;
+} sd_type_t;
 
-esp_err_t sd_mount(sd_handle_t sd, char* mp, spi_host_device_t host);
-esp_err_t sd_file_create(sd_handle_t sd, const char* file_name);
-esp_err_t sd_file_write();
+typedef sd_type_t* sd_handle_t;
+
+sd_err_t sd_init(const sd_config_t* config, sd_handle_t sd);
+sd_err_t sd_deinit(sd_handle_t sd);
+
+sd_err_t sd_mount(sd_handle_t sd);
+sd_err_t sd_unmount(sd_handle_t sd);
+
 
 #ifdef __cplusplus
 }
